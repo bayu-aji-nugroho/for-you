b1991b27dee804567be8aecb247bceb8395c903f
Bayu Aji Nugroho <152474822+bayu-aji-nugroho@users.noreply.github.com>
2024-10-18 22:49:48 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
